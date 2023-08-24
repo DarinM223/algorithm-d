bb0326c464ccdfed01cf066a5420158277593c8b
d <darinm223@gmail.com>
2023-08-23 18:26:45 -0700

    Update generic and build script

--- build_polyml.sh
+++ build_polyml.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cat > build.sml <<EOL
 structure Unsafe =
 struct
@@ -66,11 +67,50 @@ fun useProject root' file =
     use file;
     OS.FileSys.chDir root
   end;
+(* Uncomment this and put library files in here to prevent reloading them each time. *)
+(*
+PolyML.SaveState.loadState "save" handle _ => (
+PolyML.SaveState.saveState "save" );
+*)
 EOL
 
 mlton -stop f algorithmd.mlb \
     | grep -v ".mlb" \
-    | grep -v "/usr/local/lib/mlton/sml/basis/" \
-    | grep -v "/usr/local/lib/mlton/targets/" \
-    | while read line ; do echo "use \"$line\";" ; done \
+    | grep -v "/lib/mlton/sml/basis/" \
+    | grep -v "/lib/mlton/targets/" \
+    | while read line ; do \
+     if [[ $line == *.mlton.sml ]] ; then \
+       if [ -f "${line/%.mlton.sml/.polyml.sml}" ]; then \
+         echo "use \"${line/%.mlton.sml/.polyml.sml}\";" ; \
+       elif [ -f "${line/%.mlton.sml/.default.sml}" ]; then \
+         echo "use \"${line/%.mlton.sml/.default.sml}\";" ; \
+       elif [ -f "${line/%.mlton.sml/.common.sml}" ]; then \
+         echo "use \"${line/%.mlton.sml/.common.sml}\";" ; \
+       elif [ -f "${line/%.mlton.sml/.sml}" ]; then \
+         echo "use \"${line/%.mlton.sml/.sml}\";" ; \
+       fi \
+     elif [[ $line == *.mlton.fun ]] ; then \
+       if [ -f "${line/%.mlton.fun/.polyml.fun}" ]; then \
+         echo "use \"${line/%.mlton.fun/.polyml.fun}\";" ; \
+       elif [ -f "${line/%.mlton.fun/.default.fun}" ]; then \
+         echo "use \"${line/%.mlton.fun/.default.fun}\";" ; \
+       elif [ -f "${line/%.mlton.fun/.common.fun}" ]; then \
+         echo "use \"${line/%.mlton.fun/.common.fun}\";" ; \
+       elif [ -f "${line/%.mlton.fun/.fun}" ]; then \
+         echo "use \"${line/%.mlton.fun/.fun}\";" ; \
+       fi \
+     elif [[ $line == *.mlton.sig ]] ; then \
+       if [ -f "${line/%.mlton.sig/.polyml.sig}" ]; then \
+         echo "use \"${line/%.mlton.sig/.polyml.sig}\";" ; \
+       elif [ -f "${line/%.mlton.sig/.default.sig}" ]; then \
+         echo "use \"${line/%.mlton.sig/.default.sig}\";" ; \
+       elif [ -f "${line/%.mlton.sig/.common.sig}" ]; then \
+         echo "use \"${line/%.mlton.sig/.common.sig}\";" ; \
+       elif [ -f "${line/%.mlton.sig/.sig}" ]; then \
+         echo "use \"${line/%.mlton.sig/.sig}\";" ; \
+       fi \
+     else\
+       echo "use \"$line\";" ; \
+     fi \
+    done \
     >> build.sml
